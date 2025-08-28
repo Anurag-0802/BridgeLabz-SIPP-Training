b1a397841a5d441652a4943b5dca5fe25d2db240
Anurag-0802 <coder080205@gmail.com>
2025-08-28 08:05:49 +0530

    Exceptions

---
+++ Exceptions/bankTransactionSystem.java
@@ -0,0 +1,34 @@
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+class bankTransactionSystem {
+    double balance = 1000;
+
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+}
+
+public class bankTransactionSystem {
+    public static void main(String[] args) {
+        bankTransactionSystem account = new bankTransactionSystem();
+
+        try {
+            account.withdraw(1500);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
